03f522108867cb32a555a21bec8044d1c5304b7b
LucaFumasoli <Luca.fumasoli@samtrevano.ch>
2022-02-24 09:34:08 +0100

    commit del 24.02.2021

---
+++ 5_Applicativo/GestioneViaggi/application/views/utenteNonLoggato/Tratta.php
@@ -0,0 +1,113 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+ 
+    <title>Ricerca Viaggi</title>
+
+    <meta charset="UTF-8">
+    <meta name="keywords" content="HTML,CSS,XML,JavaScript">
+    <meta name="author" content="Luca Fumasoli I2C">
+    <meta name="description" content="">
+
+  <!--Chrome e windows 10-->
+  <!--data creazione: 10.02.2021 data ultima modifica: 25.11.2021-->
+
+<script src="myscripts.js"></script>
+<link rel="stylesheet" type="text/css" href="./style.css">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</head>
+
+<style>
+.arrow {
+    color:  blue;
+}
+
+.time {
+    font-size: 10px;
+}
+</style>
+
+<body style="background-color: #424242; color: white;">
+
+<div>
+    <form method="post">
+        <div class="row">
+            <div class="col-2">
+                <span>442 </span>
+            </div>
+            <div class="col-4">
+                <span>15 posti disponibili</span>
+            </div>
+            <br>
+            <br>
+        </div>
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-2 text-center">
+                <span>Lugano</span><br>
+                <span class="time">15:50</span>
+            </div>
+            <div class="col"></div>
+        </div>
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-2 text-center">
+                <span class="arrow" style="font-size: 50px"> &#124;</span>
+            </div>
+            <div class="col">
+                <br>
+                <span>Lamone</span><br>
+                <span class="time">15:55</span>
+            </div>
+            <div class="col"></div>
+        </div>
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-2 text-center">
+                <span class="arrow" style="font-size: 50px"> &#124;</span>
+            </div>
+            <div class="col">
+                <div class="pull-left">
+                    <br>
+                    <span>Taverne-Torricella</span><br>
+                    <span class="time">15:58</span>
+                </div>
+            </div>
+            <div class="col"></div>
+        </div>
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-2 text-center">
+                <span class="arrow" style="font-size: 50px"> &#124;</span>
+            </div>
+            <div class="col">
+                <br>
+                <span>Mezzovico</span><br>
+                <span class="time">16:02</span>
+            </div>
+            <div class="col"></div>
+        </div>
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-2 text-center">
+                <span class="arrow" style="font-size: 50px">&darr;</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-2 text-center">
+                <span>Rivera-Bironico</span><br>
+                <span class="time">16:05</span>
+            </div>
+            <div class="col"></div>
+        </div>
+    </div>
+    </form>
+    <br>
+</div>
+
+</body>
+</html>
