7e5587d2ee5078da7c25228a60657454e8a7ae2c
LucaFumasoli <Luca.fumasoli@samtrevano.ch>
2022-03-10 16:27:23 +0100

    commit del 10.03.2021

--- 5_Applicativo/GestioneViaggi/application/views/CreaBusView.php
+++ 5_Applicativo/GestioneViaggi/application/views/CreaBusView.php
@@ -1,12 +1,37 @@
+<?php 
+
+	if (session_status() === PHP_SESSION_NONE) { 
+		session_start(); 
+	}
+
+	if (isset($_GET['grandezza'])) {
+        $grand = $_GET['grandezza'];
+        $_SESSION['grand'] = $grand;
+    }else {
+        $grand = 1;
+    }
+    if ($_SESSION['grand'] == 0) {
+        $numRig = 5;
+    } else if ($_SESSION['grand'] == 1) {
+        $numRig = 10;
+    }else {
+        $numRig = 15;
+    }
+
+    if (isset($_POST['nomeLinea'])) {
+       	$_SESSION['colorRed'] = true;
+    }
+
+?>
 
 	<div class="row">
 		<div class="col-2"></div>
 		<div class="col-1">
-			<span style="color: white;">numero posti</span>
+			<span style="color: white;">Grandezza bus</span>
 		</div>
 
 		<div class="col-1">
-			<form action=" <?php echo URL . 'home/creaBus' ?> " method="get">
+			<form action=" <?php echo URL . 'home/createBus' ?> " method="get">
 				<select name="grandezza" id="grandezza" onblur="document.form2.input.value = this.value;">
 				  <option value="0">Piccolo</option>
 				  <option value="1">Medio</option>
@@ -15,17 +40,22 @@
 				<input type="submit" value="conferma">
 			</form>
 			<script type="text/javascript">
-				document.getElementById('grandezza').value = "<?php echo $grand;?>";
+				document.getElementById('grandezza').value = "<?php echo $_SESSION['grand'];?>";
 			</script>
 			<sub>
 		</div>
 		<div class="col-2"></div>
 		<div class="col-1">
-			<form action=" <?php echo URL . 'home/creaNuovoBus' ?> " method="get">
+			<form action=" <?php echo URL . 'home/tryToCreateBus' ?> " method="post">
+			<input type="hidden" value="<?php echo $grand ?>" name="grand" />
       		<span style="color: white;">nome linea</span>
 		</div>
 		<div class="col-1">
-			<input type="number" name="nomeLinea">
+			<?php if($_SESSION['colorRed'] == false){ ?>
+	      		<input type="number" name="nomeLinea" id="nomeLinea" value="<?php echo $_SESSION['nomeLinea']; ?>">
+	      	<?php }else { ?>
+	      		<input type="number" name="nomeLinea" id="nomeLinea" style="border: 2px solid red">
+	      	<?php } ?>
 		</div>
 		<div class="col"></div>
 	</div>
